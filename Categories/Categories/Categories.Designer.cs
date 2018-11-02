0e2656c6de1eb5f1d3e56c14c2e12854fa5ad64a
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2018-11-02 16:22:35 +0200

    Categories adding

--- Categories/Categories/Categories.Designer.cs
+++ Categories/Categories/Categories.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -51,16 +58,87 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(209, 391);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(210, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add new category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Title";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select parent category";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(452, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(533, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Categories
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Categories";
             this.Text = "Categories";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +146,12 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Categories/Categories/Categories.cs
+++ Categories/Categories/Categories.cs
@@ -28,21 +28,33 @@ namespace Categories
             public int GetId() { return Id; }
             public int GetParentId() { return parenId; }
             public string GetName() { return Name; }
+            public DateTime GetCreated() { return Created; }
+
+            public override string ToString()
+            {
+                return string.Format("{0}", Name);
+            }
         }
 
         private static int Count;
-        static List<Category> cat = new List<Category>();
+        static List<Category> catSet = new List<Category>();
+        Category emptyItem = new Category(-1, -1, "", DateTime.Now);
 
         public Categories()
         {
-            InitializeComponent();
+            catSet.Add(new Category(Count++, -1, "cat", DateTime.Now));
+            catSet.Add(new Category(Count++, -1, "cat", DateTime.Now));
+            catSet.Add(new Category(Count++, 0, "sub-cat", DateTime.Now));
+            catSet.Add(new Category(Count++, 1, "sub-cat", DateTime.Now));
+            catSet.Add(new Category(Count++, 2, "sub-sub-cat", DateTime.Now));
+            catSet.Add(new Category(Count++, 4, "sub-sub-sub-cat", DateTime.Now));
 
-            cat.Add(new Category(Count++, -1, "Parent Category", DateTime.Now));
-            cat.Add(new Category(Count++, -1, "Parent Category", DateTime.Now));
-            cat.Add(new Category(Count++, 0, "Sub Category", DateTime.Now));
-            cat.Add(new Category(Count++, 1, "Sub Category", DateTime.Now));
-            cat.Add(new Category(Count++, 2, "Sub Sub Category", DateTime.Now));
-            cat.Add(new Category(Count++, 4, "Sub Sub Category", DateTime.Now));
+            InitializeComponent();
+            
+            comboCat.Add(emptyItem);
+            FillComboBox("");
+            comboBox1.DataSource = comboCat;
+            comboBox1.SelectedItem = emptyItem;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -54,15 +66,51 @@ namespace Categories
         // Recursive
         void RecursiveCategoryTree(string subMark, int parentId = -1)
         {
-            List<Category> query = cat.Where(x => x.GetParentId() == parentId).ToList();
+            List<Category> query = catSet.Where(x => x.GetParentId() == parentId).ToList();
             if (query.Count > 0)
             {
                 foreach (var row in query)
                 {
-                    richTextBox1.AppendText(subMark + "- " + row.GetName() + "\n");
-                    RecursiveCategoryTree(subMark + "   ", row.GetId());
+                    richTextBox1.AppendText(subMark + "-" + row.GetName() + "\n");
+                    RecursiveCategoryTree(subMark + "  ", row.GetId());
                 }
             }
+        }
+
+        List<Category> comboCat = new List<Category>();
+
+        private void FillComboBox(string subMark, int parentId = -1)
+        {
+            List<Category> query = catSet.Where(x => x.GetParentId() == parentId).ToList();
+            if (query.Count > 0)
+            {
+                foreach (var row in query)
+                {
+                    Category cCat = new Category(row.GetId(), row.GetParentId(), subMark + "-" + row.GetName(), row.GetCreated());
+                    comboCat.Add(cCat);
+                    FillComboBox(subMark + "  ", row.GetId());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string title = textBox1.Text;
+            Category selectedCat = (Category)comboBox1.SelectedItem;
+            int parentId = selectedCat.GetId();
+            catSet.Add(new Category(Count++, parentId, title, DateTime.Now));
+
+            richTextBox1.Clear();
+            RecursiveCategoryTree("");
+            comboBox1.DataSource = comboCat;
+            textBox1.Text = null;
+            comboBox1.SelectedItem = emptyItem;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            comboBox1.SelectedItem = emptyItem;
         }
     }
 }
